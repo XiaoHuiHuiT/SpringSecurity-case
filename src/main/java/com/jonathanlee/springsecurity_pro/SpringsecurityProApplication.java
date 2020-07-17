504da6a4c3b1f29b06739f54ff8810392521d45a
JonathanLee <leadertangbn@gmail.com>
2020-07-17 19:59:25 +0800

    Spring Security 初体验

---
+++ src/main/java/com/jonathanlee/springsecurity_pro/SpringsecurityProApplication.java
@@ -0,0 +1,13 @@
+package com.jonathanlee.springsecurity_pro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsecurityProApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsecurityProApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jonathanlee/springsecurity_pro/SpringsecurityProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonathanlee.springsecurity_pro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityProApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
